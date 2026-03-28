df70a147bfa5c62b57c84aad6b9cf3a289ab941b
Thalen Vassen <chef.attila@gmail.com>
2026-03-28 20:04:09 +0100

    függvények és visszatérő értékek

--- 011_functions.py
+++ 011_functions.py
@@ -3,8 +3,77 @@ def hello():
     print("Hello Python")
 hello()
 
+#-----------------------------------
 #írj egyyfügvényt ami kap egyszámot és kiírjaa négyzetét
 def square(number):
     print(number*number)
 number=int(input("Adj meg egy számot és megmondom a négyzetét! "))
 square(number)
+
+#-----------------------------------
+#Írj egy függvényt, ami kap két számot és vissza adja az összegüket (visszatérési értékkel)
+def square_return(number1, number2):
+    return number1+number2
+
+number1=int(input("Adj meg egy számot: "))
+number2=int(input("Adj meg egy másik számot: "))
+
+numbers_sum=square_return(number1,number2)
+print(numbers_sum)
+
+#-----------------------------------
+#Írj egy fügvényt, ami kap egy listát és visszadja a legnagyobb értéket
+def max_number(numbers):
+    for i, value in enumerate(numbers):
+        if i==0:
+            high_number=numbers[0]
+        elif high_number<numbers[i]:
+            high_number=numbers[i]
+    return(high_number)
+
+numbers = [1,2,3,5,7,9,10,1,2,5]
+high_number=max_number(numbers)
+
+print("A", numbers, "listában a legnagyobbszám a:", high_number)
+
+#-----------------------------------
+#Írj egy fügvényt, ami kap egy listát és visszadja a legnagyobb értéket ----- MÁSIK MEGOLDÁS
+def max_number(numbers):
+    high_number=numbers[0]
+    for i in numbers:
+        if high_number<i:
+            high_number=i
+    return(high_number)
+
+numbers = [1,2,3,5,7,9,10,1,2,5]
+high_number=max_number(numbers)
+
+print("A", numbers, "listában a legnagyobbszám a:", high_number)
+
+#-----------------------------------
+#Írj egy fügvényt, ami kap egy listát és visszadja hány páros szám van benne
+def even_number(numbers):
+    even_number_count=0
+    for i in numbers:
+        if i%2==0:
+            even_number_count+=1
+    return(even_number_count)
+
+numbers = [1,2,3,5,7,9,10,1,2,5]
+even_number_count=even_number(numbers)
+
+print("A", numbers, "listában", even_number_count,"db páros számtalálható")
+
+#-----------------------------------
+#Írj egy fügvényt, ami kap egy listát és visszadja a páros számok összegét
+def even_number(numbers):
+    even_number_sum=0
+    for i in numbers:
+        if i%2==0:
+            even_number_sum+=i
+    return(even_number_sum)
+
+numbers = [1,2,3,5,7,9,10,1,2,5]
+even_number_sum=even_number(numbers)
+
+print("A", numbers, "listában", even_number_sum,"a párosos számok összege")
--- 0_Gyakorlás.py
+++ 0_Gyakorlás.py
@@ -1,11 +1,49 @@
-#Írj egy függvényt, ami kap két számot és vissza adja az összegüket (visszatérési értékkel)
+#-----------------------------------
+#Írj három fügvényt, amik kapnak egy-egy listát és visszadja a páros számok számát, összegét és a legnagyobbat
 
-def square_return(number1, number2):
-    return number1+number2
+#lista
+numbers = [1,21,3,5,7,9,101,1,21,5]
 
-number1=int(input("Adj meg egy számot: "))
-number2=int(input("Adj meg egy másik számot: "))
+#Páros számok száma
+def count_even(numbers):
+    darab =  0
+    for i in numbers:
+        if i%2==0:
+            darab+=1
+    return darab
 
-numbers_sum=square_return(number1,number2)
+#Páros számok összege
+def sum_even(numbers):
+    osszeg = 0
+    for i in numbers:
+        if i%2==0:
+            osszeg= osszeg+i
+    return osszeg
 
-print(numbers_sum)
+#Legnagyobb páros szám
+def max_even(numbers):
+    ElsoParosSzam=True
+    for i in numbers:
+        if ElsoParosSzam:
+            if i%2==0:
+                maximum=i  
+                ElsoParosSzam = False
+        elif i%2==0 and i>maximum:
+            maximum=i
+    if ElsoParosSzam:
+         return None      
+    else:
+        return maximum
+
+#Vizsgálat
+darab = count_even(numbers)
+osszeg = sum_even(numbers)
+
+#Eredmény
+print("A", numbers, "listában", darab, "db páros szám van.")
+print("A", numbers, "listában", osszeg, "a páros számok összege.")
+maximum = max_even(numbers)
+if maximum is None:
+    print("A", numbers, "listában nincs páros szám.")       
+else:
+    print("A", numbers, "listában", maximum, "a legnagyobb páros szám.")
