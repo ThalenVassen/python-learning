e05c78a63529a80914d8ab86a7ed3c136ebc3c5c
Thalen Vassen <chef.attila@gmail.com>
2026-03-31 06:35:29 +0200

    1-szer melyik számfordulelő

--- 012_dict_basic.py
+++ 012_dict_basic.py
@@ -75,3 +75,60 @@ def most_frequent(numbers):
 max_key=most_frequent(numbers)
 
 print(max_key)
+
+#-----------------------------------
+#Írj egy függvényt, ami kap egy listát és dict segítségével megmondja melyik szám szerepel a legtöbbször,d9ntetlenesetén None
+numbers=[1,2,2,2]
+
+def most_frequent(numbers):
+    max_value = 0
+    max_key=None
+    count={}
+    dontetlen =False
+    for i in numbers:
+        if i in count:
+            count[i]+=1
+        else:
+            count[i]=1
+    for key,value in count.items():
+        if value == max_value:
+            dontetlen =True
+        elif value > max_value:
+            max_value=value
+            max_key = key
+            dontetlen =False
+    if dontetlen:
+        return None
+    else:
+        return max_key
+
+max_key=most_frequent(numbers)
+
+print(max_key)
+
+#-----------------------------------
+#Írj egy függvényt, ami kap egy listát és dict segítségével megmondja melyik szám szerepel a legtöbbször - gGET használata
+numbers=[1,2,1,3,2,1]
+
+def most_frequent(numbers):
+    max_value = 0
+    max_key=None
+    count={}
+    dontetlen =False
+    for i in numbers:
+        count[i]=count.get(i,0)+1
+    for key,value in count.items():
+        if value == max_value:
+            dontetlen =True
+        elif value > max_value:
+            max_value=value
+            max_key = key
+            dontetlen =False
+    if dontetlen:
+        return None
+    else:
+        return max_key
+
+max_key=most_frequent(numbers)
+
+print(max_key)
